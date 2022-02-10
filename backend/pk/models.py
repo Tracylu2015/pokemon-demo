60cce1d426f24674c0644486cb388e60d9b8ac03
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2022-02-10 12:58:33 -0800

    Add backend project and models

---
+++ backend/pk/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=60)
+    password = models.CharField()
+
+class UserFavorite(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    favorite_pk_id = models.IntegerField()
+
+class UserNote(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    notes = models.TextField()
